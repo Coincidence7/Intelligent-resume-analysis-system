6596ab4ea22b83603621b2aca8915d74ef5ff46d
David205x <David205x@outlook.com>
2023-07-02 22:17:40 +0800

    页面框架添加
    移除了fontawesome组件
    使用了element-ui-plus，需要终端npm install
    仅一点点前端，功能函数待实现

--- web/src/main.js
+++ web/src/main.js
@@ -1,6 +1,16 @@
+// import { createApp } from 'vue'
+// import App from './App.vue'
+// import router from './router'
+// import store from './store'
+
+// createApp(App).use(ElementUI).use(store).use(router).mount('#app')
+
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import './style/common.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(ElementPlus).use(store).use(router).mount('#app')
